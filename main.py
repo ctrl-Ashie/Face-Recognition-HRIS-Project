d41dc63ea9c6c0e5add7c5b3afe15a246ed90000
Ashley <238839298+ctrl-Ashie@users.noreply.github.com>
2026-02-23 16:18:36 +0800

    feat: add title and  menu button layout

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import tkinter 
-window = tkinter.Tk()
+import tkinter as tk
+window = tk.Tk()
 window.title("Login Monitor")
-window.minsize(400, 300)
+window.minsize(700, 500)
+
+font_py = ("Arial", 19, "bold")
+
+first_label = tk.Label(text="Login Monitor", font=font_py)
+first_label.pack(side="top")
+
+def menu_clicked(  ):
+    print("filler")
+    
+
+menu_button = tk.Button(text="Menu",command=menu_clicked)
+menu_button.pack()
 
 window.mainloop()
